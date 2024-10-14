c70324ec47a3b5ca3d30fbd8ffa42ef4694dae0a
Khamvang <108178100+Khamvang@users.noreply.github.com>
2024-10-14 08:24:33 +0700

    Update HC_happy_call.sql

--- HC_happy_call.sql
+++ HC_happy_call.sql
@@ -1,14 +1,17 @@
 
 
 -- __________________________________________ Prepare new Month __________________________________________
--- check and delete the customer who doesnn't like LALCO
+-- 1) check and delete the customer who doesnn't like LALCO
 select * from tabSME_BO_and_Plan where reason_of_credit = '18 ບໍ່ມັກ LALCO';
 delete from tabSME_BO_and_Plan where reason_of_credit = '18 ບໍ່ມັກ LALCO';
+delete from tabSME_BO_and_Plan_bk where reason_of_credit = '18 ບໍ່ມັກ LALCO';
 
 
--- SABC update the list for next month
+-- 2) SABC update the list for next month
 delete from temp_sme_pbx_BO where type in ('S', 'A', 'B', 'C');
 
+
+-- 3) insert and replace SABC rank from tabSME_BO_and_Plan to temp_sme_pbx_BO
 replace into temp_sme_pbx_BO
 select bp.name `id`, bp.customer_tel, null `pbx_status`, null `date`, bp.staff_no `current_staff`, 
 	case when bp.rank_update in ('S', 'A', 'B', 'C') then bp.rank_update else bp.rank1 end `type`, 
@@ -20,9 +23,11 @@ where ( (bp.rank1 in ('S', 'A', 'B', 'C') and bp.rank_update not in ('FFF') )
 	and bp.contract_status not in ('Contracted');
 
 
--- F update the list for next month
+-- 4) F update the list for next month
 delete from temp_sme_pbx_BO where type in ('F');
 
+
+-- 5) insert and replace F rank from tabSME_BO_and_Plan to temp_sme_pbx_BO
 insert into temp_sme_pbx_BO
 select bp.name `id`, bp.customer_tel, null `pbx_status`, null `date`, bp.staff_no `current_staff`, 
 	case when bp.rank_update in ('S', 'A', 'B', 'C') then bp.rank_update else 'F' end `type`, 
@@ -33,3 +38,9 @@ where ( (bp.rank1 in ('F') and bp.rank_update not in ('FFF') )
 	or bp.rank_update in ('F') )
 	and bp.name not in (select id from temp_sme_pbx_BO where type in ('S', 'A', 'B', 'C') )
 	and bp.contract_status not in ('Contracted');
+
+
+-- 6) Export data to allocate the cases of resigned employees to current employees
+
+
+
