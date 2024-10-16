7640c94590d9ba1e52a67a39988c5725d5f7f4ef
Khamvang <108178100+Khamvang@users.noreply.github.com>
2024-10-16 23:36:37 +0700

    Update temp_PBX.sql

--- temp_PBX.sql
+++ temp_PBX.sql
@@ -133,7 +133,23 @@ select * from temp_sme_pbx_sp;
 
 
 -- 6) update Dor and Inc
-update temp_dor_inc set customer_tel = concat(90, customer_tel);
+update temp_dor_inc set customer_tel = 
+	case when customer_tel = '' then ''
+		when (length (regexp_replace(customer_tel , '[^[:digit:]]', '')) = 11 and left (regexp_replace(customer_tel , '[^[:digit:]]', ''),3) = '020')
+			or (length (regexp_replace(customer_tel , '[^[:digit:]]', '')) = 10 and left (regexp_replace(customer_tel , '[^[:digit:]]', ''),2) = '20')
+			or (length (regexp_replace(customer_tel , '[^[:digit:]]', '')) = 8 and left (regexp_replace(customer_tel , '[^[:digit:]]', ''),1) in ('2','5','7','8','9'))
+		then concat('9020',right(regexp_replace(customer_tel , '[^[:digit:]]', ''),8)) -- for 020
+		when (length (regexp_replace(customer_tel , '[^[:digit:]]', '')) = 10 and left (regexp_replace(customer_tel , '[^[:digit:]]', ''),3) = '030')
+			or (length (regexp_replace(customer_tel , '[^[:digit:]]', '')) = 9 and left (regexp_replace(customer_tel , '[^[:digit:]]', ''),2) = '30')
+			or (length (regexp_replace(customer_tel , '[^[:digit:]]', '')) = 7 and left (regexp_replace(customer_tel , '[^[:digit:]]', ''),1) in ('2','4','5','7','9'))
+		then concat('9030',right(regexp_replace(customer_tel , '[^[:digit:]]', ''),7)) -- for 030
+		when left (right (regexp_replace(customer_tel , '[^[:digit:]]', ''),8),1) in ('0','1','') then concat('9030',right(regexp_replace(customer_tel , '[^[:digit:]]', ''),7))
+		when left (right (regexp_replace(customer_tel , '[^[:digit:]]', ''),8),1) in ('2','5','7','8','9')
+		then concat('9020',right(regexp_replace(customer_tel , '[^[:digit:]]', ''),8))
+		else concat('9020',right(regexp_replace(customer_tel , '[^[:digit:]]', ''),8))
+	end
+;
+
 
 update temp_dor_inc tdi join pbx_unique pu on (tdi.customer_tel = pu.contact_no)
 set tdi.pbx_status = pu.status, tdi.`date` = pu.date_created 
