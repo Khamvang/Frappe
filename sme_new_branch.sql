91b467258271f5b5b6d005667d2fee5433deb9e6
Khamvang <108178100+Khamvang@users.noreply.github.com>
2024-10-18 15:05:13 +0700

    Update sme_new_branch.sql

--- sme_new_branch.sql
+++ sme_new_branch.sql
@@ -15,20 +15,46 @@ create table `sme_new_branch`(
 
 
 
--- 3) Insert data to table 
+-- 3) Insert data to table in Frappe server
 insert into tabSME_Approach_list
 (`creation`, `modified`, `modified_by`, `owner`, `docstatus`, `idx`, `staff_no`, `callcenter_of_sales`, `double_count`, `type`, `is_sales_partner`, `approch_list`, `visit_or_not`, `reason_of_visit`, `visit_date`, `details_of_visit_reason`, `priority_to_visit`, `visit_location_url`, `visit_time`, `evidence`, `disbursement_date_pay_date`, `usd_loan_amount`, `usd_fee_amount`, `monthly_interest_rate`, `normal_bullet`, `contract_no`, `case_no`, `customer_name`, `customer_tel`, `address_province_and_city`, `address_village`, `rank1`, `rank_update`, `customer_card`, `attach_customer_card`, `ringi_status`, `ringi_comment`, `contract_status`, `contract_comment`, `maker`, `model`, `year`, `usd_value`, `estate_types`, `land_area`, `real_estate_province_and_city`, `village`, `owner_first_name`, `owner_last_name`, `who_is_it`, `usd_value_of_real_estate`, `have_or_not`, `from_whom`, `date`, `usd_value_of_receivable`, `remark`, `sp_cc`, `rank_update_sp_cc`, `sp_cc_remark`, `credit`, `rank_of_credit`, `reason_of_credit`, `credit_remark`, `visit_checker`, `actual_visit_or_not`, `comment_by_visit_checker`, `_user_tags`, `_comments`, `_assign`, `_liked_by`, `priority`, `branch_name`, `approach_type`, `approach_id`
 )
-select `creation`, `modified`, `modified_by`, `owner`, `docstatus`, `idx`, `staff_no`, `callcenter_of_sales`, `double_count`, `type`, 
+select `creation`, `modified`, `modified_by`, `owner`, `docstatus`, `idx`, `staff_no`, `callcenter_of_sales`, `double_count`, bp.`type`, 
 	`is_sales_partner`, `approch_list`, `visit_or_not`, `reason_of_visit`, `visit_date`, `details_of_visit_reason`, `priority_to_visit`, 
 	`visit_location_url`, `visit_time`, `evidence`, `disbursement_date_pay_date`, `usd_loan_amount`, `usd_fee_amount`, `monthly_interest_rate`, 
-	`normal_bullet`, `contract_no`, `case_no`, `customer_name`, `customer_tel`, `address_province_and_city`, `address_village`, `rank1`, `rank_update`, 
+	`normal_bullet`, `contract_no`, `case_no`, `customer_name`, bp.`customer_tel`, `address_province_and_city`, `address_village`, `rank1`, `rank_update`, 
 	`customer_card`, `attach_customer_card`, `ringi_status`, `ringi_comment`, `contract_status`, `contract_comment`, `maker`, `model`, `year`, 
 	`usd_value`, `estate_types`, `land_area`, `real_estate_province_and_city`, `village`, `owner_first_name`, `owner_last_name`, `who_is_it`, 
-	`usd_value_of_real_estate`, `have_or_not`, `from_whom`, `date`, `usd_value_of_receivable`, `remark`, `sp_cc`, `rank_update_sp_cc`, `sp_cc_remark`, 
+	`usd_value_of_real_estate`, `have_or_not`, `from_whom`, bp.`date`, `usd_value_of_receivable`, `remark`, `sp_cc`, `rank_update_sp_cc`, `sp_cc_remark`, 
 	`credit`, `rank_of_credit`, `reason_of_credit`, `credit_remark`, `visit_checker`, `actual_visit_or_not`, `comment_by_visit_checker`, `_user_tags`, 
-	`_comments`, `_assign`, `_liked_by`, null `priority`, nb.`branch_name`, nb.`approach_type`, nb.approach_id 
-from tabSME_BO_and_Plan bp inner join sme_new_branch nb on (bp.name = nb.approach_id)
+	`_comments`, `_assign`, `_liked_by`, null `priority`, 
+	case when bp.address_province_and_city = 'Vientiane Province - Vangvieng' then 'Vangvieng'
+		when bp.address_province_and_city = 'Vientiane Capital - Parkngum' then 'Mayparkngum'
+		when bp.address_province_and_city = 'Vientiane Capital - Xaythany' then 'Xaythany'
+		when bp.address_province_and_city = 'Vientiane Capital - Hadxayfong' then 'Hadxaifong'
+		when bp.address_province_and_city = 'Vientiane Capital - Naxaythong' then 'Naxaiythong'
+		when substring_index(bp.address_province_and_city, ' ', 1) = 'Phongsaly' then 'Phongsary'
+		when bp.address_province_and_city = 'Luangprabang - Nam Bak' then 'Nambak'
+		when substring_index(bp.address_province_and_city, ' ', 1) = 'Xaysomboune' then 'Xaisomboun'
+		when bp.address_province_and_city = 'Xiengkhuang - Kham' then 'Kham'
+		when bp.address_province_and_city = 'Xayaboury - Parklai' then 'Parklai'
+		when bp.address_province_and_city = 'Savanakhet - Songkhone' then 'Songkhone'
+		when bp.address_province_and_city = 'Savanakhet - Phine' then 'Phine'
+		when bp.address_province_and_city = 'Borikhamxay - Khamkeut' then 'Khamkeuth'
+		when bp.address_province_and_city = 'Champasack - Phonthong' then 'Chongmeg'
+		when bp.address_province_and_city = 'Champasack - Paksong' then 'Paksxong'
+		when substring_index(bp.address_province_and_city, ' ', 1) = 'Sekong' then 'Sekong'
+		else null
+	end as `branch_name`, 
+	case when tb.`type` in ('S', 'A', 'B', 'C') then 'SABC'
+		when tb.`type` in ('F') then 'F'
+		else null
+	end as `approach_type`, 
+	bp.name as `approach_id` 
+from tabSME_BO_and_Plan bp inner join temp_sme_pbx_BO tb on (bp.name = tb.id)
+where bp.address_province_and_city in ('Vientiane Province - Vangvieng', 'Vientiane Capital - Parkngum', 'Vientiane Capital - Xaythany', 'Vientiane Capital - Hadxayfong', 'Vientiane Capital - Naxaythong', 'Luangprabang - Nam Bak', 'Xiengkhuang - Kham', 'Xayaboury - Parklai', 'Savanakhet - Songkhone', 'Savanakhet - Phine', 'Borikhamxay - Khamkeut', 'Champasack - Phonthong', 'Champasack - Paksong')
+	or substring_index(bp.address_province_and_city, ' ', 1) in ('Phongsaly', 'Xaysomboune', 'Sekong')
+;
 
 
 -- 4) update sales
