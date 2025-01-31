ce3259c2075de5e9d1aa277ea61db1a0574ff502
Khamvang CHUESA <108178100+Khamvang@users.noreply.github.com>
2025-01-31 19:26:29 +0700

    Update HC_happy_call_SABCF.sql

--- HC_happy_call_SABCF.sql
+++ HC_happy_call_SABCF.sql
@@ -1,5 +1,19 @@
 
 
+-- Create table 
+CREATE TABLE `temp_sme_pbx_BO` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`customer_tel` varchar(255) DEFAULT NULL,
+	`pbx_status` varchar(255) DEFAULT NULL,
+	`date` datetime DEFAULT NULL,
+	`current_staff` varchar(255) DEFAULT NULL,
+	`type` varchar(255) DEFAULT NULL,
+	`month_type` int(11) DEFAULT NULL COMMENT '3=3 months or less, 6=6months or less, 9=9months or less, 12=12months or less',
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+
+
 -- __________________________________________ Prepare new Month __________________________________________
 -- 1) check and delete the customer who doesnn't like LALCO
 select * from tabSME_BO_and_Plan where reason_of_credit = '18 ບໍ່ມັກ LALCO';
