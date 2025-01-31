cdd57205bb8fe6a8a04b662a54ec46edef84f259
Khamvang CHUESA <108178100+Khamvang@users.noreply.github.com>
2025-01-31 23:57:26 +0700

    Update LCC_campaign.sql

--- LCC_campaign.sql
+++ LCC_campaign.sql
@@ -55,6 +55,24 @@ order by address_province_and_city asc;
 
 
 
+-- G rank list for all each province and district
+select name 'id', customer_tel 'contact_no', customer_name 'name', 
+	left(address_province_and_city, locate(' -', address_province_and_city)-1) 'province_eng', null 'province_laos',
+	right(address_province_and_city, (length(address_province_and_city) - locate('- ', address_province_and_city) -1 ) ) 'district_eng', null 'district_laos',
+	address_village 'village', 
+	case when contract_status = 'Contracted' then 'Contracted'
+		when contract_status != 'Contracted' and rank_update in ('S', 'A', 'B', 'C') then 'SABC'
+		when contract_status != 'Contracted' and rank_update in ('F', 'FF1', 'FF2', 'FFF') then 'F'
+		when contract_status != 'Contracted' and rank_update in ('G') then 'G'
+	end 'type', 
+	maker, model, `year`, 2 as 'remark_1', null 'remark_2', null 'remark_3'
+from tabSME_BO_and_Plan  
+where contract_status != 'Contracted' and address_province_and_city != '' 
+	and (contract_status != 'Contracted' and rank_update in ('G') )
+order by address_province_and_city asc;
+
+
+
 -- prepare list of all XYZ of province and district
 select name, broker_name, broker_tel, address_province_and_city, broker_type
 from tabsme_Sales_partner
